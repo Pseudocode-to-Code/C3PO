109289591ab4fa1f590b12371cae1906ffc939cf
Vibha Masti <vsmasti@gmail.com>
2022-08-31 02:55:29 +0000

    Inference with tokenizer

--- copy_generation/pytorch_models/transformer_inference_copy.py
+++ copy_generation/pytorch_models/transformer_inference_copy.py
@@ -3,21 +3,21 @@ import argparse
 import os
 
 import torch
-from transformers import T5ForConditionalGeneration, T5Tokenizer
+from transformers import T5ForConditionalGeneration, RobertaTokenizerFast
 import pickle
 from tqdm import tqdm
 import time
 
 from vanilla_seq2seq import *
 from attention_seq2seq import *
-from dataloader import *
+from transformer_dataloader import *
 
 parser = argparse.ArgumentParser(description="Seq2Seq Training")
 parser.add_argument('--non-copy', '-n', default=False, action='store_true', help='Train on non-copy dataset')
 parser.add_argument('--checkpoint', '-c', default = 9, help='Training resumption. Pass the epoch number from which to resume')
 args = parser.parse_args()
 
-model_type = 'transformer_new_pad'
+model_type = 'transformer_fixed_tokenizer'
 
 # Read CPY dataset
 data = pd.read_pickle('../../data/CPY_dataset_new.pkl')
@@ -25,80 +25,80 @@ data = pd.read_pickle('../../data/CPY_dataset_new.pkl')
 MAXLEN = 74 # Got from experimentation.ipynb
 eval_data = pd.read_pickle('../../data/CPY_dataset_eval_tree_copy.pkl')
 
-pseudo_full_voc = None
-pseudo_copy_voc = None
+# pseudo_full_voc = None
+# pseudo_copy_voc = None
 
-try:
-    # Read vocabulary from pickle file
-    with open('../../data/vocabs/pseudo_full.pkl', 'rb') as f:
-        pseudo_full_voc = pickle.load(f)
+# try:
+#     # Read vocabulary from pickle file
+#     with open('../../data/vocabs/pseudo_full.pkl', 'rb') as f:
+#         pseudo_full_voc = pickle.load(f)
 
-    with open('../../data/vocabs/pseudo_copy.pkl', 'rb') as f:
-        pseudo_copy_voc = pickle.load(f)
+#     with open('../../data/vocabs/pseudo_copy.pkl', 'rb') as f:
+#         pseudo_copy_voc = pickle.load(f)
 
-except FileNotFoundError:
-    eval_data = pd.read_pickle('../../data/CPY_dataset_eval_tree_copy.pkl')
-    train_data = pd.read_pickle('../../data/CPY_dataset_new.pkl')
+# except FileNotFoundError:
+#     eval_data = pd.read_pickle('../../data/CPY_dataset_eval_tree_copy.pkl')
+#     train_data = pd.read_pickle('../../data/CPY_dataset_new.pkl')
 
-    pseudo_full_voc = Vocabulary('train pseudocode')
-    pseudo_copy_voc = Vocabulary('train pseudo with cpy')
+#     pseudo_full_voc = Vocabulary('train pseudocode')
+#     pseudo_copy_voc = Vocabulary('train pseudo with cpy')
 
-    pseudo_full_voc.build_vocabulary(train_data, 'pseudo_token')
-    pseudo_copy_voc.build_vocabulary(train_data, 'pseudo_gen_seq')
+#     pseudo_full_voc.build_vocabulary(train_data, 'pseudo_token')
+#     pseudo_copy_voc.build_vocabulary(train_data, 'pseudo_gen_seq')
 
 
     
-    # Save vocabularies to pickle
-    if not os.path.exists('../../data/vocabs'):
-        os.makedirs('../../data/vocabs')
+#     # Save vocabularies to pickle
+#     if not os.path.exists('../../data/vocabs'):
+#         os.makedirs('../../data/vocabs')
 
-    with open('../../data/vocabs/pseudo_full.pkl', 'wb') as f:
-        pickle.dump(pseudo_full_voc, f)
+#     with open('../../data/vocabs/pseudo_full.pkl', 'wb') as f:
+#         pickle.dump(pseudo_full_voc, f)
 
-    with open('../../data/vocabs/pseudo_copy.pkl', 'wb') as f:
-        pickle.dump(pseudo_copy_voc, f)
+#     with open('../../data/vocabs/pseudo_copy.pkl', 'wb') as f:
+#         pickle.dump(pseudo_copy_voc, f)
 
-if args.non_copy:
-    pseudo_voc_size = len(pseudo_full_voc)
-    model_type += '_noncopy'
-else:
-    pseudo_voc_size = len(pseudo_copy_voc)
-    model_type += '_copy'
+# if args.non_copy:
+#     pseudo_voc_size = len(pseudo_full_voc)
+#     model_type += '_noncopy'
+# else:
+#     pseudo_voc_size = len(pseudo_copy_voc)
+#     model_type += '_copy'
 
-code_voc = None
+# code_voc = None
 
-if args.non_copy:
-    try:
-        # Read vocabulary from pickle file
-        with open('../../data/vocabs/code_voc_full.pkl', 'rb') as f:
-            code_voc = pickle.load(f)
+# if args.non_copy:
+#     try:
+#         # Read vocabulary from pickle file
+#         with open('../../data/vocabs/code_voc_full.pkl', 'rb') as f:
+#             code_voc = pickle.load(f)
 
-    except FileNotFoundError:
-        code_voc = Vocabulary('code_full')
-        code_voc.build_vocabulary(train_data, 'code_token_aug')
+#     except FileNotFoundError:
+#         code_voc = Vocabulary('code_full')
+#         code_voc.build_vocabulary(train_data, 'code_token_aug')
 
-        # Save vocabularies to pickle
-        with open('../../data/vocabs/code_voc_full.pkl', 'wb') as f:
-            pickle.dump(code_voc, f)
+#         # Save vocabularies to pickle
+#         with open('../../data/vocabs/code_voc_full.pkl', 'wb') as f:
+#             pickle.dump(code_voc, f)
 
-else:
-    try:
-        # Read vocabulary from pickle file
-        with open('../../data/vocabs/code_voc_copy.pkl', 'rb') as f:
-            code_voc = pickle.load(f)
-    except FileNotFoundError:
-        code_voc = Vocabulary('code_copy')
-        code_voc.build_vocabulary(train_data, 'code_gen_seq_aug')
+# else:
+#     try:
+#         # Read vocabulary from pickle file
+#         with open('../../data/vocabs/code_voc_copy.pkl', 'rb') as f:
+#             code_voc = pickle.load(f)
+#     except FileNotFoundError:
+#         code_voc = Vocabulary('code_copy')
+#         code_voc.build_vocabulary(train_data, 'code_gen_seq_aug')
 
-        # Save vocabularies to pickle
-        with open('../../data/vocabs/code_voc_copy.pkl', 'wb') as f:
-            pickle.dump(code_voc, f)
+#         # Save vocabularies to pickle
+#         with open('../../data/vocabs/code_voc_copy.pkl', 'wb') as f:
+#             pickle.dump(code_voc, f)
 
 # Model hyperparameters
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-input_size_encoder = pseudo_voc_size
-input_size_decoder = len(code_voc)
-output_size = len(code_voc)
+# input_size_encoder = pseudo_voc_size
+# input_size_decoder = len(code_voc)
+# output_size = len(code_voc)
 
 batch_size = 8
 weight_decay = 0.01
@@ -107,25 +107,26 @@ weight_decay = 0.01
 step = 0
 
 if args.non_copy:
-    test_dataset = TestDataset(eval_data, 'pseudo_token', pseudo_full_voc)
+    tokenizer_name = './codet5-small_noncopy'
+    tokenizer = RobertaTokenizerFast.from_pretrained(f"./models/{tokenizer_name}/")
+    test_dataset = TestDataset(eval_data, 'pseudo_token', tokenizer)
+    
 else:
-    test_dataset = TestDataset(eval_data, 'pseudo_gen_seq', pseudo_copy_voc) 
+    tokenizer_name = './codet5-small_copy'
+    tokenizer = RobertaTokenizerFast.from_pretrained(f"./models/{tokenizer_name}/")
+    test_dataset = TestDataset(eval_data, 'pseudo_gen_seq', tokenizer) 
 
-print(len(test_dataset.source_vocab.stoi))
+
+model_checkpoint = "./Salesforce/codet5-small"
+model = T5ForConditionalGeneration.from_pretrained(model_checkpoint)
+model.resize_token_embeddings(len(tokenizer))
+model.to(device)
+
 
 test_loader = get_test_loader(test_dataset)
 print('No. of samples', len(test_loader))
 
-model_checkpoint = "./t5-small"
-model = T5ForConditionalGeneration.from_pretrained(model_checkpoint)
-
-tokenizer = T5Tokenizer.from_pretrained('t5-small')
-tokenizer.add_tokens(RESERVED_TOKENS)
-model.resize_token_embeddings(len(tokenizer))
-
-model.to(device)
-
-pad_idx = code_voc.stoi["[PAD]"]
+# pad_idx = code_voc.stoi["[PAD]"]
 # criterion = nn.CrossEntropyLoss(ignore_index=pad_idx)
 
 # Loading checkpoint
@@ -133,27 +134,62 @@ print(f'Loading checkpoint: {model_type}/{args.checkpoint}.tar')
 resume_checkpoint = torch.load(f'./checkpoints/{model_type}/{args.checkpoint}.tar', map_location=device) # CHANGE BASED ON CASE
 model.load_state_dict(resume_checkpoint['state_dict'])
 
-if args.non_copy:
-    pseudo_voc = pseudo_full_voc
-else:
-    pseudo_voc = pseudo_copy_voc
+# if args.non_copy:
+#     pseudo_voc = pseudo_full_voc
+# else:
+#     pseudo_voc = pseudo_copy_voc
 
 model.eval()
 
+final_seqs = []
+code_gen_seqs = []
 
 for batch_idx, batch in enumerate(tqdm(test_loader, unit='batch')):
         # Get input and targets and get to cuda
         inp_data = batch.to(dtype=torch.int64, device=device)
         # print(inp_data)
 
-        unks = torch.where(inp_data == pseudo_voc.stoi['[UNK]'])[0]
-        if len(unks) > 0:
-            inp_data[unks] = pseudo_voc.stoi['[CPY]']
+        unks = torch.where(inp_data == tokenizer.encode('[UNK]')[0])
+        print('unks', unks)
+
+        if len(unks) > 0 and not args.non_copy:
+            inp_data[unks] = tokenizer.encode('[CPY]')[0]
 
         outputs = model.generate(input_ids=inp_data)
         print(outputs)
         
+        # gen_seq = [code_voc.stoi["[START]"]] 
+    
 
+        # # Get the actual tokens that were converted to CPY
+        # copy_seq = eval_data['dt_copy_seq'][batch_idx]
+        # actual_pseudo = eval_data['pseudo_token'][batch_idx]
+        # cpy_indexes = np.where(copy_seq == 1)[0]
+
+        # # Number of CPY tags encountered
+        # cpy_cnt = 0
+
+        # for i, token_idx in enumerate(outputs):
+            
+        #     if not args.non_copy:
+        #         if token_idx == tokenizer('[CPY]').input_ids[0]:
+        #             actual_token_index = cpy_indexes[cpy_cnt]
+        #             pseudo_token = actual_pseudo[actual_token_index]
+
+        #             outputs[i] = pseudo_token
+
+
+        #             cpy_cnt += 1
+
+        #         else:
+        #             gen_seq.append(token_idx)
+
+        #     else:
+        #         gen_seq.append(token_idx)
+
+
+                
+                    
 
 
 # for batch_idx, batch in enumerate(tqdm(test_loader, unit='lines')):
